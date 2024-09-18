424d1cf42986f3bcbf1ca381eb4845b7353068f9
Ali-Barzegari-d <ali.barzegari.1401@gmail.co>
2024-09-19 02:58:44 +0330

    add

---
+++ 8-structs/5-methods/code.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+func (authInfo authenticationInfo) getBasicAuth() string {
+	return "Authorization: Basic " + authInfo.username + ":" + authInfo.password
+}
+
+func test(authInfo authenticationInfo) {
+	fmt.Println(authInfo.getBasicAuth())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(authenticationInfo{
+		username: "Google",
+		password: "12345",
+	})
+	test(authenticationInfo{
+		username: "Bing",
+		password: "98765",
+	})
+	test(authenticationInfo{
+		username: "DDG",
+		password: "76921",
+	})
+}
