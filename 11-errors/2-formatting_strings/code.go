720084905166be29484899d206ecfbddc26fb420
Ali-Barzegari-d <ali.barzegari.1401@gmail.co>
2024-09-19 03:01:07 +0330

    add files

---
+++ 11-errors/2-formatting_strings/code.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent",
+		cost,
+		recipient,
+	)
+}
+
+func test(cost float64, recipient string) {
+	s := getSMSErrorString(cost, recipient)
+	fmt.Println(s)
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(1.4, "+1 (435) 555 0923")
+	test(2.1, "+2 (702) 555 3452")
+	test(32.1, "+1 (801) 555 7456")
+	test(14.4, "+1 (234) 555 6545")
+}
